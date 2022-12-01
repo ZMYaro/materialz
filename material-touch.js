507ecd4e292be6ab5ed65396ae8b6e492fae5f01
Zachary Yaro <zmyaro@zmyaro.com>
2022-12-01 16:19:23 -0500

    Convert material-touch.js from incorrect encoding to UTF-8

--- material-touch.js
+++ material-touch.js
@@ -24,7 +24,7 @@
 	}
 	
 	/**
-	 * Add the ìactiveî class to the touched element
+	 * Add the ‚Äúactive‚Äù class to the touched element
 	 * @param {TouchEvent} e
 	 */
 	function makeActive(e) {
@@ -39,7 +39,7 @@
 	}
 	
 	/**
-	 * Remove the ìactiveî class from the touched element
+	 * Remove the ‚Äúactive‚Äù class from the touched element
 	 * @param {TouchEvent} e
 	 */
 	function makeInactive(e) {
